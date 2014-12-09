b88f641cbf5598b4ac5c28c5d56c3cc22af414cd
Justin Herrick <justin@justinherrick.com>
2014-12-09 11:05:54 -0500

    ~

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,13 +10,22 @@ require('scripts/views/*');
 require('scripts/router');
 
 WalmartSearch.IndexController = Ember.ArrayController.extend({
+    results: [],
     actions: {
         search: function () {
-            var url = 'https://sheltered-earth-7471.herokuapp.com/walmart/search?apikey=t5k2mrf5py2s5nsgd3yg4usd&query=';
+            //var url  = 'https://sheltered-earth-7471.herokuapp.com/walmart/search?apikey=t5k2mrf5py2s5nsgd3yg4usd&query=';
+            var url  = '/walmart/search?apikey=t5k2mrf5py2s5nsgd3yg4usd&query=';
+            var that = this;
             Ember.$.getJSON(url + this.get('query')).then(function (data) {
-                console.log(data);
                 console.table(data);
-                this.set('results', data);
+                data = {
+                    items: [
+                        {
+                        name: 'foo',
+                        shortDescription: 'yo yo yo'}
+                    ]
+                };
+                that.set('results', data);
             });
         }
     }
